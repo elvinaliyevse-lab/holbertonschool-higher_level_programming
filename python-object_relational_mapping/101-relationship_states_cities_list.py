bf503b82c87163c4f0cdaf98854a3436a98f8083
elvinaliyevse-lab <elvin.aliyev.se@gmail.com>
2026-07-12 18:23:37 +0400

    sd

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""
-Module for fetching all states from the database using SQLAlchemy ORM.
-"""
+"""Module to list all States and corresponding Cities"""
+from relationship_city import City
+from relationship_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
-from model_state import Base, State
-
-# Run only executed
+# Run only when executed
 if __name__ == "__main__":
-
-    # Engine creation with mysql and mysqldb DBAPI
+    # Creating the engine
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(argv[1], argv[2], argv[3]))
 
-    # Creating all classes in DB
-    Base.metadata.create_all(engine)
-
-    # Creating Session and its instance
+    # Creating the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Printing the result
+    # Printing
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-
-    # Closing the session
-    if session:
-        session.close()
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
